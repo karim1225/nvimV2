aefc45ff8d08617298c37a8ae11fbe68e2d14ec2
karim1225 <ghasemikarim1225@gmail.com>
2024-02-14 16:05:49 +0330

    update

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -286,7 +286,7 @@ M.telescope = {
     ["<leader>pt"] = { "<cmd> Telescope terms <CR>", "Pick hidden term" },
 
     -- theme switcher
-    ["<leader>th"] = { "<cmd> Telescope themes <CR>", "Nvchad themes" },
+    ["<leader>tH"] = { "<cmd> Telescope themes <CR>", "Nvchad themes" },
 
     ["<leader>ma"] = { "<cmd> Telescope marks <CR>", "telescope bookmarks" },
   },
